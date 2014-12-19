8275c5a006bbfbad5fa06ee58c1856cdaaefe77f
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-19 18:45:26 +0400

    [symbol] added

--- pdobject.py
+++ pdobject.py
@@ -80,8 +80,8 @@ class PdObject(PdBaseObject):
         if self.name() in ("bang", "b"):
             return [self.XLET_MESSAGE]
 
-        # [f], [float], [i], [int]
-        if self.name() in ("float", "f", "int", "i"):
+        # [f], [float], [i], [int], [symbol]
+        if self.name() in ("float", "f", "int", "i", "symbol"):
             return [self.XLET_MESSAGE, self.XLET_MESSAGE]
 
         # [s] or [send]
@@ -109,8 +109,8 @@ class PdObject(PdBaseObject):
         if self.name() in ("bang", "b"):
             return [self.XLET_MESSAGE]
 
-        # [f], [float], [i], [int]
-        if self.name() in ("float", "f", "int", "i"):
+        # [f], [float], [i], [int], [symbol]
+        if self.name() in ("float", "f", "int", "i", "symbol"):
             return [self.XLET_MESSAGE]
 
         # [r]

3d79476781d0a3ffeda43cef4fa1f7e0c902b684
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-22 00:02:14 +0400

    Layout class added

---
+++ layout.py
@@ -0,0 +1,205 @@
+# /usr/bin/env python
+
+# Copyright (C) 2014 by Serge Poltavski                                 #
+#   serge.poltavski@gmail.com                                            #
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 3 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   This program is distributed in the hope that it will be useful,       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program. If not, see <http://www.gnu.org/licenses/>   #
+
+
+# -*- coding: utf-8 -*-
+
+__author__ = 'Serge Poltavski'
+
+import copy
+
+
+class LayoutItem(object):
+    def __init__(self, x=0, y=0, width=0, height=0):
+        self._x = x
+        self._y = y
+        self._w = width
+        self._h = height
+
+    def x(self):
+        return self._x
+
+    def y(self):
+        return self._y
+
+    def width(self):
+        return self._w
+
+    def height(self):
+        return self._h
+
+    def brect(self):
+        return (self._x, self._y, self._w, self._h)
+
+    def __str__(self):
+        return "(%i,%i,%ix%i)" % (self._x, self._y, self._w, self._h)
+
+    def right(self):
+        return self._x + self._w
+
+    def bottom(self):
+        return self._y + self._h
+
+    def top(self):
+        return self._y
+
+    def left(self):
+        return self._x
+
+    def move_by(self, x, y):
+        self._x += x
+        self._y += y
+
+
+class Layout(object):
+    HORIZONTAL, VERTICAL = range(1, 3)
+
+    def __init__(self, direction, distance=0):
+        self._parent = None
+        self._elements = []
+        self._dir = direction
+        self._distance = distance
+
+    def parent(self):
+        return self._parent
+
+    def set_parent(self, parent):
+        self._parent = parent
+
+    def count(self):
+        return len(self._elements)
+
+    def _add_vertical(self, item):
+        if self.count() > 0:
+            prev_item = self._elements[-1]
+            item.move_by(0, prev_item.bottom() + self._distance)
+
+        self._elements.append(item)
+
+    def _add_horizontal(self, item):
+        if self.count() > 0:
+            prev_item = self._elements[-1]
+            item.move_by(prev_item.right() + self._distance, 0)
+
+        self._elements.append(item)
+
+    def add_item(self, item):
+        assert item != self
+
+        if item in self._elements:
+            item2 = copy.copy(item)
+            return self.add_item(item2)
+
+        assert isinstance(item, LayoutItem)
+        if self._dir == self.HORIZONTAL:
+            self._add_horizontal(item)
+        elif self._dir == self.VERTICAL:
+            self._add_vertical(item)
+        else:
+            assert False
+
+    def add_layout(self, layout):
+        assert layout != self
+
+        if layout in self._elements:
+            layout2 = copy.deepcopy(layout)
+            return self.add_layout(layout2)
+
+        if self.count() > 0:
+            prev_item = self._elements[-1]
+
+            if self._dir == self.HORIZONTAL:
+                layout.move_by(prev_item.right(), 0)
+            elif self._dir == self.VERTICAL:
+                layout.move_by(0, prev_item.bottom())
+            else:
+                assert False
+
+        self._elements.append(layout)
+
+
+    def item(self, num):
+        assert num < len(self._elements)
+        return self._elements[num]
+
+
+    def brect(self):
+        if self.count() == 0:
+            return (0, 0, 0, 0)
+
+        left = []
+        top = []
+        right = []
+        bottom = []
+
+        for item in self._elements:
+            br = item.brect()
+            left.append(br[0])
+            top.append(br[1])
+            right.append(br[0] + br[2])
+            bottom.append(br[1] + br[3])
+
+        x0 = min(left)
+        y0 = min(top)
+        x1 = max(right)
+        y1 = max(bottom)
+        return (x0, y0, x1 - x0, y1 - y0)
+
+    def bottom(self):
+        br = self.brect()
+        return br[1] + br[3]
+
+    def right(self):
+        br = self.brect()
+        return br[0] + br[2]
+
+    def move_by(self, x, y):
+        for item in self._elements:
+            item.move_by(x, y)
+
+    def __str__(self):
+        res = ""
+        if self._dir == self.HORIZONTAL:
+            s = ""
+            for item in self._elements:
+                s += "[%i:%ix%i] " % (item.x(), item.width(), item.height())
+
+            s = s.strip()
+            res += "-" * (len(s) + 4)
+            res += "\n| %s |\n" % (s)
+            res += "-" * (len(s) + 4)
+        elif self._dir == self.VERTICAL:
+            col_len = 0
+            els = []
+            for item in self._elements:
+                els.append("[%i:%ix%i]" % (item.y(), item.width(), item.height()))
+                col_len = max(col_len, len(els[-1]))
+
+            res += "-" * (col_len + 4)
+            res += "\n"
+            fmt = "| %%%ds |\n" % (col_len)
+            for e in els:
+                res += fmt % (e)
+            res += "-" * (col_len + 4)
+
+        return res
+
+
+
+
+
---
+++ tests/test_layout.py
@@ -0,0 +1,179 @@
+# /usr/bin/env python
+
+# Copyright (C) 2014 by Serge Poltavski                                 #
+#   serge.poltavski@gmail.com                                            #
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 3 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   This program is distributed in the hope that it will be useful,       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program. If not, see <http://www.gnu.org/licenses/>   #
+
+
+# -*- coding: utf-8 -*-
+
+__author__ = 'Serge Poltavski'
+
+import unittest
+from layout import *
+
+
+class TestLayout(unittest.TestCase):
+    def test_parent(self):
+        l = Layout(Layout.HORIZONTAL)
+        self.assertEqual(l.parent(), None)
+        l2 = Layout(Layout.VERTICAL)
+        l2.set_parent(l)
+        self.assertEqual(l2.parent(), l)
+
+    def test_count(self):
+        l = Layout(Layout.HORIZONTAL)
+        i = LayoutItem()
+        self.assertEqual(l.count(), 0)
+        l.add_item(i)
+        self.assertEqual(l.count(), 1)
+
+    def test_add_item(self):
+        l = Layout(Layout.HORIZONTAL)
+        i = LayoutItem()
+        l.add_item(i)
+        self.assertEqual(l.item(0), i)
+        i2 = LayoutItem()
+        l.add_item(i2)
+        self.assertEqual(l.item(0), i)
+        self.assertEqual(l.item(1), i2)
+
+        lv = Layout(Layout.VERTICAL)
+        iv = LayoutItem()
+        lv.add_item(iv)
+        self.assertEqual(lv.item(0), iv)
+
+    def test_add_vitem(self):
+        lv = Layout(Layout.VERTICAL)
+        i = LayoutItem(0, 0, 10, 10)
+        lv.add_item(i)
+        self.assertEqual(lv.item(0).brect(), (0, 0, 10, 10))
+        lv.add_item(i)
+        self.assertEqual(lv.item(0).brect(), (0, 0, 10, 10))
+        self.assertEqual(lv.item(1).brect(), (0, 10, 10, 10))
+        lv.add_item(i)
+        lv.add_item(i)
+        self.assertEqual(lv.item(3).brect(), (0, 30, 10, 10))
+        self.assertEqual(lv.brect(), (0, 0, 10, 40))
+        lv.add_item(LayoutItem(0, 0, 20, 20))
+        self.assertEqual(lv.brect(), (0, 0, 20, 60))
+
+        lh = Layout(Layout.HORIZONTAL)
+        ih = LayoutItem(0, 0, 10, 10)
+        lh.add_item(ih)
+        self.assertEqual(lh.item(0).brect(), (0, 0, 10, 10))
+        lh.add_item(ih)
+        self.assertEqual(lh.item(0).brect(), (0, 0, 10, 10))
+        self.assertEqual(lh.item(1).brect(), (10, 0, 10, 10))
+        lh.add_item(ih)
+        self.assertEqual(lh.item(2).brect(), (20, 0, 10, 10))
+
+    def test_move_by(self):
+        lv = Layout(Layout.VERTICAL)
+        i = LayoutItem(0, 0, 10, 10)
+        lv.add_item(i)
+        lv.add_item(i)
+        lv.move_by(10, 10)
+        self.assertEqual(lv.item(0).brect(), (10, 10, 10, 10))
+        self.assertEqual(lv.item(1).brect(), (10, 20, 10, 10))
+
+
+    def test_brect(self):
+        lv = Layout(Layout.VERTICAL)
+        self.assertEqual(lv.brect(), (0, 0, 0, 0))
+
+    def test_nested(self):
+        lv = Layout(Layout.VERTICAL)
+        lh = Layout(Layout.HORIZONTAL)
+        lv.add_layout(lh)
+        lv.add_layout(lh)
+
+        self.assertNotEqual(lv.item(0), lv.item(1))
+
+    def test_add_layout(self):
+        lv = Layout(Layout.VERTICAL)
+        iv = LayoutItem(0, 0, 100, 100)
+        lv.add_item(iv)
+
+        lh = Layout(Layout.HORIZONTAL)
+        ih = LayoutItem(0, 0, 50, 50)
+        lh.add_item(ih)
+        lh.add_item(ih)
+        lh.add_item(ih)
+        self.assertEqual(lh.item(0).brect(), (0, 0, 50, 50))
+        self.assertEqual(lh.item(1).brect(), (50, 0, 50, 50))
+        self.assertEqual(lh.item(2).brect(), (100, 0, 50, 50))
+
+        lv.add_layout(lh)
+        self.assertEqual(lh.item(0).brect(), (0, 100, 50, 50))
+        self.assertEqual(lh.item(1).brect(), (50, 100, 50, 50))
+        self.assertEqual(lh.item(2).brect(), (100, 100, 50, 50))
+
+    def test_add_vlayout(self):
+        lh = Layout(Layout.HORIZONTAL)
+        ih = LayoutItem(0, 0, 50, 50)
+        lh.add_item(ih)
+        lh.add_item(ih)
+        lh.add_item(ih)
+        self.assertEqual(lh.item(0).brect(), (0, 0, 50, 50))
+        self.assertEqual(lh.item(1).brect(), (50, 0, 50, 50))
+        self.assertEqual(lh.item(2).brect(), (100, 0, 50, 50))
+
+        lv = Layout(Layout.VERTICAL)
+        iv = LayoutItem(0, 0, 100, 100)
+        lv.add_item(iv)
+
+        lh.add_layout(lv)
+        self.assertEqual(iv.brect(), (150, 0, 100, 100))
+
+
+    def test_str(self):
+        lh = Layout(Layout.HORIZONTAL)
+        i = LayoutItem(0, 0, 10, 10)
+        lh.add_item(i)
+        lh.add_item(i)
+        lh.add_item(i)
+
+        s = str(lh)
+
+        lv = Layout(Layout.HORIZONTAL)
+        iv = LayoutItem(0, 0, 10, 10)
+        lv.add_item(iv)
+
+        s = str(lv)
+
+
+class TestLayoutItem(unittest.TestCase):
+    def test_str(self):
+        li = LayoutItem(1, 2, 3, 4)
+        s = str(li)
+        self.assertEqual("(1,2,3x4)", s)
+        self.assertEqual(1, li.left())
+        self.assertEqual(1, li.x())
+        self.assertEqual(2, li.top())
+        self.assertEqual(3, li.width())
+        self.assertEqual(4, li.height())
+        self.assertEqual(4, li.right())
+        self.assertEqual(6, li.bottom())
+
+    def test_move_by(self):
+        li = LayoutItem(10, 20, 30, 40)
+        li.move_by(1, 2)
+        self.assertEqual((11, 22, 30, 40), li.brect())
+
+
+if __name__ == "__main__":
+    unittest.main()
+

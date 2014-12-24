7640d9695868d3bb093058fb718edfd833106b78
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-24 14:12:08 +0400

    #!/usr/bin/env added

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,4 +1,4 @@
-# /usr/bin/env python
+#!/usr/bin/env python
 
 # Copyright (C) 2014 by Serge Poltavski                                 #
 #   serge.poltavski@gmail.com                                            #

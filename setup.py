313d32ee59b4207c5d1e9ce9df88fe5446f7c379
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-26 03:57:25 +0400

    setup fix

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@
 
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 setup(name='pddoc',
-      version='0.1',
+      version='0.1.3',
       description='PureData documentation tools',
       long_description=readme(),
       classifiers=[
-            'Development Status :: 2 - Pre-Alpha',
-            'Topic :: Software Development :: Documentation',
-            'Topic :: Multimedia :: Sound/Audio',
-            'Programming Language :: Python :: 2.7',
-            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+          'Development Status :: 2 - Pre-Alpha',
+          'Topic :: Software Development :: Documentation',
+          'Topic :: Multimedia :: Sound/Audio',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
       ],
       url='https://github.com/uliss/pddoc',
       author='Serge Poltavski',
@@ -29,5 +31,8 @@ setup(name='pddoc',
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
+      entry_points={
+          'console_scripts': ['pd2image=pddoc.pd2image:main']
+      },
       include_package_data=True,
       zip_safe=False)

94edd94fe6feded990b0a81e4ea70c7026233a6e
Serge Poltavski <serge.poltavski@gmail.com>
2014-12-25 23:46:25 +0400

    setup skeleton added

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name='pddoc',
+      version='0.1',
+      description='PureData documentation tools',
+      url='https://github.com/uliss/pddoc',
+      author='Serge Poltavski',
+      author_email='serge.poltavski@gmail.com',
+      license='GPLv3',
+      packages=['pddoc'],
+      zip_safe=False)

87068cd104fd4748cf32603f61d026a09c871709
Serge Poltavski <serge.poltavski@gmail.com>
2015-01-26 03:32:38 +0400

    manifest change

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 
 setup(name='pddoc',
-      version='0.1.5',
+      version='0.1.5b',
       description='PureData documentation tools',
       long_description=readme(),
       classifiers=[

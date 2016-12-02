d63e987a375b997488b0a921ff39e838a24f831b
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-03 02:09:43 +0300

    default in-memory db added

--- pddoc/pd/xletcalculator.py
+++ pddoc/pd/xletcalculator.py
@@ -21,6 +21,7 @@ __author__ = 'Serge Poltavski'
 
 from xlettextdatabase import XletTextDatabase
 from xletcalcdatabase import XletCalcDatabase
+from xletdatabase import XletMemoryDatabase
 from xletpatchlookup import XletPatchLookup
 from . import EXTERNALS_DIR
 import os
@@ -30,7 +31,9 @@ class XletCalculator(object):
     def __init__(self, dbname=None):
         ext_dir = EXTERNALS_DIR
 
-        self._dbs = []
+        self.mem_db = XletMemoryDatabase("default_memory")
+
+        self._dbs = [self.mem_db]
         self._dbs.append(XletTextDatabase(os.path.join(ext_dir, 'core/pd_objects.db'), "core"))
         self._dbs.append(XletCalcDatabase(os.path.join(ext_dir, 'core/xletsdb_core.py'), "core"))
 

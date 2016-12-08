a21327f61841014707fc9a4c75e42f21e18b32bc
Serge Poltavski <serge.poltavski@gmail.com>
2016-12-09 02:46:38 +0300

    version update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 
 setup(name='pddoc',
-      version='0.2.9',
+      version='0.3.0',
       description='PureData documentation tools',
       long_description=readme(),
       classifiers=[

ac9c45b2eb5c3391a2a2d3038c6c895d677d4efc
Serge Poltavski <serge.poltavski@gmail.com>
2023-08-24 03:26:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='pddoc',
-      version='0.7.4',
+      version='0.7.5',
       description='PureData documentation tools',
       long_description=readme(),
       classifiers=[

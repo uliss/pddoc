c1c15493827ca39f0bbb3997460b9ab82dfeae27
Serge Poltavski <serge.poltavski@gmail.com>
2025-04-23 01:36:14 +0300

    Update constants.py

--- pddoc/pd/constants.py
+++ pddoc/pd/constants.py
@@ -1,4 +1,8 @@
 import os.path as path
 
-XLET_MESSAGE, XLET_SOUND, XLET_GUI, XLET_IGNORE = range(0, 4)
+XLET_MESSAGE: int = 0
+XLET_SOUND: int = 1
+XLET_GUI: int = 2
+XLET_IGNORE: int = 3
+
 EXTERNALS_DIR = path.join(path.dirname(__file__), "externals")

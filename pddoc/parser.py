631b6a7b8dec77a4a1b728d2c85ebc34b1baa57f
Serge Poltavski <serge.poltavski@gmail.com>
2025-05-06 17:58:46 +0300

    Update parser.py

--- pddoc/parser.py
+++ pddoc/parser.py
@@ -50,11 +50,12 @@ def parse_xml(path):
             # manual XInclude implementation with template substitution
             prop_xml = etree.parse(dirname + "/" + ref.attrib["href"])
             if ref.attrib.get("subst", "true") != "false":
-                tmpl = Template(prop_xml.getroot().text)
-                obj_name = os.path.basename(path)[:-6]  # strip .pddoc extension
-                obj_name = obj_name.replace("~", "").replace(".", "_")
-                txt = tmpl.substitute(obj=obj_name)
-                prop_xml.getroot().text = txt
+                for tr in prop_xml.getroot():
+                    tmpl = Template(tr.text)
+                    obj_name = os.path.basename(path)[:-6]  # strip .pddoc extension
+                    obj_name = obj_name.replace("~", "").replace(".", "_")
+                    txt = tmpl.substitute(obj=obj_name)
+                    tr.text = txt
 
             new_tag = prop_xml.getroot()
             ref.getparent().replace(ref, new_tag)

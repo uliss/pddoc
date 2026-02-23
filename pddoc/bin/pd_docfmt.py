a7cc74a52c68e73bef5c38478b1899b5ed2c7c3e
Serge Poltavski <serge.poltavski@gmail.com>
2026-02-24 00:49:50 +0300

    Update pd_docfmt.py

--- pddoc/bin/pd_docfmt.py
+++ pddoc/bin/pd_docfmt.py
@@ -4,8 +4,6 @@ from __future__ import print_function
 
 import argparse
 
-from lxml import etree
-
 #   Copyright (C) 2023 by Serge Poltavski                                 #
 #   serge.poltavski@gmail.com                                             #
 #                                                                         #
@@ -24,6 +22,8 @@ from lxml import etree
 
 __author__ = 'Serge Poltavski'
 
+from pddoc.parser import PddocFormatParser
+
 
 def main():
     arg_parser = argparse.ArgumentParser(description='format pddoc file')
@@ -33,92 +33,20 @@ def main():
     args = vars(arg_parser.parse_args())
     in_file = args['name']
 
-    # dirname = os.path.dirname(in_file)
-    parser = etree.XMLParser(resolve_entities=False,
-                             strip_cdata=False,
-                             remove_blank_text=False)
-    xml = etree.parse(in_file, parser)
-
-    if not xml:
+    pddoc = PddocFormatParser(in_file)
+    if not pddoc.is_ok():
         exit(1)
 
-    def tag_index(name):
-        return (
-            "title", "meta", "info", "mouse", "arguments", "properties", "methods", "inlets", "outlets",
-            "example").index(
-            name)
-
-    pddoc = xml.getroot()
-    pddoc_version = pddoc.get("version", "1.0")
-    obj = pddoc.getchildren()[0]
-    for r in obj.iter("object"):
-        r[:] = sorted(r, key=lambda x: tag_index(x.tag))
-
-    # strip @lang finished=true
-    for tr in pddoc.findall("**/description/tr[@finished='true']"):
-        tr.attrib.pop("finished")
-
-    # strip blanks
-    for elem in obj.iter('*'):
-        if elem.tag not in ("method", "property", "pdascii") and elem.text is not None:
-            elem.text = elem.text.strip()
-
-        if elem.tag == "pdascii":
-            txt = elem.text.rstrip().lstrip("\n")
-            elem.text = etree.CDATA("\n" + txt + "\n")
-
-    # remove old method comments
-    for comment in obj.xpath('//object/methods/comment()'):
-        comment.getparent().remove(comment)
-
-    # sort methods ny name
-    for m in obj.iter("methods"):
-        m[:] = sorted(m, key=lambda x: x.get("name", ""))
-
-    # add new comments
-    for m in obj.xpath('//object/methods/*'):
-        name = m.get("name", "")
-        # skip comments ending with '-'
-        if name == "" or name.find("--") > 0 or name.endswith('-'):
-            continue
-
-        c = etree.Comment(' ' + m.attrib["name"] + ' ')
-        m.addprevious(c)
-
-    # remove old property comments
-    for comment in obj.xpath('//object/properties/comment()'):
-        comment.getparent().remove(comment)
-
-    # sort properties by name
-    for prop in obj.iter("properties"):
-        prop[:] = sorted(prop, key=lambda x: x.get("name", ""))
-
-    # add new property comments
-    for m in obj.xpath('//object/properties/*'):
-        name = m.attrib.get("name")
-        if name is not None:
-            c = etree.Comment(f" {name} ")
-            m.addprevious(c)
-
-    pddoc = etree.Element(pddoc.tag, version=pddoc_version, nsmap={"xi": "http://www.w3.org/2001/XInclude"})
-    pddoc.insert(0, obj)
-
-    etree.indent(pddoc, space=" ", level=2)
+    pddoc.sort_sections()
+    pddoc.strip_finished_tr()
+    pddoc.strip_blanks()
+    pddoc.sort_and_comment_methods()
+    pddoc.sort_and_comment_properties()
 
     if args["in_place"]:
-        etree.ElementTree(pddoc).write(in_file,
-                                       pretty_print=True,
-                                       encoding="UTF-8",
-                                       xml_declaration=False,
-                                       method="xml",
-                                       doctype='<?xml version="1.0" encoding="utf-8"?>')
+        pddoc.save_to(in_file)
     else:
-        print(etree.tostring(pddoc,
-                             pretty_print=True,
-                             encoding="UTF-8",
-                             xml_declaration=False,
-                             method="xml",
-                             doctype='<?xml version="1.0" encoding="utf-8"?>').decode())
+        print(pddoc.to_string())
 
 
 if __name__ == '__main__':
